187c058d1851a8b7c3b19a4011cff905a4738970
Sumi Seo <sumseo@paul-f4Br4s4.clusters.42paris.fr>
2025-03-23 17:11:01 +0100

    feat/okay

--- srcs/requirements/mariadb/entrypoint.sh
+++ srcs/requirements/mariadb/entrypoint.sh
@@ -2,6 +2,7 @@
 
 echo "CREATE DATABASE IF NOT EXISTS ${DB_NAME};" >>/etc/mysql/init.sql
 echo "CREATE USER IF NOT EXISTS '${DB_ADMIN_NAME}'@'%' IDENTIFIED BY '${DB_ADMIN_PWD}';" >>/etc/mysql/init.sql
+echo "CREATE USER IF NOT EXISTS '${DB_ADMIN_NAME}'@'localhost' IDENTIFIED BY '${DB_ADMIN_PWD}';" >>/etc/mysql/init.sql
 echo "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_ADMIN_NAME}'@'%' WITH GRANT OPTION;" >>/etc/mysql/init.sql
 echo "ALTER USER 'root'@'%' IDENTIFIED BY '${DB_ROOT_PWD}';" >>/etc/mysql/init.sql
 echo "FLUSH PRIVILEGES;" >>/etc/mysql/init.sql
