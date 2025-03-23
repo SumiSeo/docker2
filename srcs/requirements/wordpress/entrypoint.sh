29a841877f5f66d251730bf0ad0c4f0ed680ff04
Sumi Seo <sumseo@paul-f4Br4s4.clusters.42paris.fr>
2025-03-23 18:58:48 +0100

    Feat/finish

--- srcs/requirements/wordpress/entrypoint.sh
+++ srcs/requirements/wordpress/entrypoint.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-cd /var/www/html
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-chmod +x wp-cli.phar
-./wp-cli.phar core download --allow-root
-./wp-cli.phar config create --dbname=wordpress --dbuser=admin --dbpass=admin --dbhost=mariadb --allow-root
-./wp-cli.phar core install --url=localhost --title=inception --admin_user=admin --admin_password=admin --admin_email=admin@admin.com --allow-root
+echo "Starting entrypoint script..."
+# Your logic here to run PHP-FPM and other services
 
 php-fpm -F
